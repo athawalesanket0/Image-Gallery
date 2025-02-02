6c98833ded1d450c2664685e7ef58510586da3e3
Sanket Athawale <athawalesanket0@gmail.com>
2025-02-02 14:32:31 +0530

    Added menu & responsiveness

--- src/components/navigationBar.js
+++ src/components/navigationBar.js
@@ -1,18 +1,24 @@
-import React from 'react';
-import {Moon, Sun, User} from 'lucide-react';
+import React, {useState} from 'react';
+import {Moon, Sun, Files, Users, User} from 'lucide-react';
 import ImageSearch from './imageSearch';
 
 const NavigationBar = ({ onSearch, darkMode, setDarkMode }) => {
+    const [menuOption, setMenuOption] = useState(false);
+    const toggleMenu = () => {
+        setMenuOption(!menuOption);
+    };
     const handleToggle = () => {
         setDarkMode(!darkMode);
     };
+    
     return (
         <nav className="navigation-bar">
             <div className="logo">Image Gallery</div>
             <ImageSearch onSearch={onSearch} />
-            <ul className="nav-links">
-                <li><a href="/collection">Collection</a></li>
-                <li><a href="/community">Community</a></li>
+            <div className="menu-icon" onClick={toggleMenu}>&#9776;</div>
+            <ul className={`nav-links ${menuOption ? 'show' : ''}`}>
+                <li><a href="/collection"><Files/></a></li>
+                <li><a href="/community"><Users/></a></li>
                 <li><a href="/profile"><User /></a></li>
                 <li onClick={handleToggle} style={{ cursor: 'pointer' }}>
                     {darkMode ? <Sun /> : <Moon />}
